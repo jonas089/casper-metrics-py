c3cdcd5117749340ee5b495620a738134453eaa6
jonas089 <jonas089@gmail.com>
2023-01-10 21:44:46 +0100

    TBD for gas_consumption

--- gas_consumption.py
+++ gas_consumption.py
@@ -3,5 +3,21 @@ from constants import ts_dp_path
 from utils.csprtime import to_date_time, to_unix
 import os
 
+'''
+    This file parses a LOCAL tsdp dataset ( see subsets.py ) to retrieve deploy hashs in a monthly fashion:
+    [
+        '2022-05':{hash1, hash2, hashn},
+        '2022-06':{...},
+        ...
+    ]
+
+    Deploys are then queried using pycspr and gas consumption metrics are applied similar to V1 of Casper-Metrics
+
+    TBD:
+        1. Test tsdp generation in ./data/tsdp/...
+        2. Code this function
+        3. Test this function
+'''
+
 _FileList = os.listdir(ts_dp_path)
 print(FileList)
