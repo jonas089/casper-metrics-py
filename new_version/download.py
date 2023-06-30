6c24e81d5971328ab511d2cd78cb2a1aead053f7
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2023-06-30 21:21:57 +0200

    tsdp first 50000 blocks

--- new_version/download.py
+++ new_version/download.py
@@ -1,15 +1,17 @@
 import pycspr, os, pickle, time
 from pycspr import NodeClient, NodeConnection
 from tqdm import tqdm
+from csprtime import dateTime, unixTime
 
 # Constants
-nodeIp = "54.180.220.20"
+nodeIp = "65.108.127.242"
 restPort = 8888
 rpcPort = 7777
 ssePort = 9999
 # Config
 basePath = './data'
-blockRange = (0,1000);
+tsdpPath = './tsdp'
+blockRange = (0,50000);
 
 # Casper Client
 client = NodeClient(NodeConnection(nodeIp, restPort, rpcPort, ssePort))
@@ -18,11 +20,13 @@ client = NodeClient(NodeConnection(nodeIp, restPort, rpcPort, ssePort))
 def createDir():
     if not os.path.exists(basePath):
         os.mkdir(basePath)
+    if not os.path.exists(tsdpPath):
+        os.mkdir(tsdpPath)
 createDir()
 
 # Main
-class BlockChain:
-    def __init__(self, blockRange, steps=100):
+class CasperMetrics:
+    def __init__(self, blockRange, steps=1000):
         self.blockRange = blockRange
         self.steps = steps
 
@@ -35,12 +39,12 @@ class BlockChain:
             processed = False
             while not processed:
                 try:
-                    block = client.get_block(n);
+                    block = client.get_block(n)
                     processed = True
+                    break
                 except Exception as error:
                     print("Network error: ", error)
                     time.sleep(0.5)
-
             fileName = "{}_{}.dat".format(r[0], r[1])
             filePath = "{}/{}".format(basePath, fileName)
 
@@ -50,15 +54,14 @@ class BlockChain:
             else:
                 blocks = []
             blocks.append(block)
-
             with open(filePath, 'wb') as file:
                 pickle.dump(blocks, file)
-
+            
+            
             n += 1
             if n == r[1] and n != blockRange[1] - 1:
                 r[0] = n + 1
                 r[1] = r[1] + self.steps
-
             progressBar.update(1)
 
     def filenames(self):
@@ -66,23 +69,38 @@ class BlockChain:
         reminder = (self.blockRange[1] - self.blockRange[0]) % self.steps
         count = int(((self.blockRange[1] - self.blockRange[0]) - reminder) / self.steps)
         for i in range(0, count):
-            name = "{}/{}_{}.dat".format(basePath, str(self.blockRange[0] + i*(self.steps)+1), str(self.blockRange[0] + i*(self.steps) + self.steps))
+            name = "{}_{}.dat".format(str(self.blockRange[0] + i*(self.steps)+1), str(self.blockRange[0] + i*(self.steps) + self.steps))
             names.append(name)
         return names
 
 
     def tsdp(self):
         fileNames = self.filenames()
-        print(fileNames)
         for f in fileNames:
-            if not os.path.exists(f):
+            filePath = "{}/{}".format(basePath, f)
+            deployHashes = {}
+            transferHashes = {}
+            tsdpFile = "{}/{}".format(tsdpPath, f)
+            if os.path.exists(tsdpFile):
                 continue
-            with open(f, 'rb') as blockFile:
+            if not os.path.exists(filePath):
+                continue
+            blocks = []
+            with open(filePath, 'rb') as blockFile:
                     blocks = pickle.loads(blockFile.read())
-                    #print(blocks)
-                    print(len(blocks))
-                    time.sleep(5)
+            for block in blocks:
+                timestamp = block['header']['timestamp']
+                if len(block['body']['deploy_hashes']) != 0:   
+                    for d in block['body']['deploy_hashes']:
+                        deployHashes[timestamp] = d
+                if len(block['body']['transfer_hashes']) != 0:   
+                    for d in block['body']['transfer_hashes']:
+                        transferHashes[timestamp] = d
+            with open(tsdpFile, 'wb') as tsdpFile:
+                pickle.dump([transferHashes, deployHashes], tsdpFile)
 
-blockchain = BlockChain(blockRange)
+blockchain = CasperMetrics(blockRange)
 #blockchain.download()
 blockchain.tsdp()
+
+#deploy_hashes, transfer_hashes
