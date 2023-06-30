cb97ca03df1c2a5781b4a2de6256865e99562e4e
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2023-06-30 15:49:14 +0200

    indexing

--- new_version/download.py
+++ new_version/download.py
@@ -22,7 +22,7 @@ createDir()
 
 # Main
 class BlockChain:
-    def __init__(self, blockRange, steps=99):
+    def __init__(self, blockRange, steps=100):
         self.blockRange = blockRange
         self.steps = steps
 
@@ -57,28 +57,29 @@ class BlockChain:
             n += 1
             if n == r[1] and n != blockRange[1] - 1:
                 r[0] = n + 1
-                r[1] = r[1] + 100
-            
+                r[1] = r[1] + self.steps
+
             progressBar.update(1)
-    
+
     def filenames(self):
         names = []
         reminder = (self.blockRange[1] - self.blockRange[0]) % self.steps
         count = int(((self.blockRange[1] - self.blockRange[0]) - reminder) / self.steps)
         for i in range(0, count):
-            name = "{}/{}_{}.dat".format(basePath, str(self.blockRange[0] + i*(self.steps+1)), str(self.blockRange[0] + i*(self.steps+1) + self.steps))
+            name = "{}/{}_{}.dat".format(basePath, str(self.blockRange[0] + i*(self.steps)+1), str(self.blockRange[0] + i*(self.steps) + self.steps))
             names.append(name)
         return names
 
 
     def tsdp(self):
         fileNames = self.filenames()
+        print(fileNames)
         for f in fileNames:
             if not os.path.exists(f):
                 continue
             with open(f, 'rb') as blockFile:
                     blocks = pickle.loads(blockFile.read())
-                    print(blocks)
+                    #print(blocks)
                     print(len(blocks))
                     time.sleep(5)
 
